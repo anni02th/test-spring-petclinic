58f0cc877e66e45035f151a484e5aac3d85cb286
anni02th <aniketmore2000th@gmail.com>
2025-08-25 09:50:03 +0530

    adding prediction

---
+++ predict.py
@@ -0,0 +1,13 @@
+# predict.py
+import random
+
+def make_prediction():
+    """
+    A dummy prediction function.
+    For this test, it will always return 'FAIL'.
+    """
+    # In the future, your real model logic will go here.
+    print("FAIL")
+
+if __name__ == "__main__":
+    make_prediction()
